b6d77ac389d16a8d3c20ffd3858d918051bde5ee
chengtsungpao <abaozheng6@gmail.com>
2022-01-23 06:24:31 +0800

    fix

--- synsin/data/realestate10k.py
+++ synsin/data/realestate10k.py
@@ -147,14 +147,14 @@ class RealEstate10K(data.Dataset):
         print("end loadtxt")
 
         # # Cheng fx
-        # import os
-        # import matplotlib.pyplot as plt
-        # imageSaveFolder = self.base_file + "/frames/%s/%s/" % (self.dataset, self.imageset[index])
-        # if not os.path.isdir(imageSaveFolder):
-        #     os.mkdir(imageSaveFolder)
-        # # im = Image.fromarray(frames)
-        # # im.save(imageSaveFolder + self.imageset[index] + ".png")
-        # plt.imsave(imageSaveFolder + self.imageset[index] + ".png", frames)
+        for line in frames:
+            time = line[0]
+            import os
+            import matplotlib.pyplot as plt
+            imageSaveFolder = self.base_file + "/frames/%s/%s/" % (self.dataset, self.imageset[index])
+            if not os.path.isdir(imageSaveFolder):
+                os.mkdir(imageSaveFolder)
+            plt.imsave(imageSaveFolder + time + ".png", np.random.rand(500, 500))
 
         image_index = self.rng.choice(frames.shape[0], size=(1,))[0]
 
