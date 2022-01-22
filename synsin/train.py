950705c338fba034a2f3a28d14f8fe928346b766
chengtsungpao <abaozheng6@gmail.com>
2022-01-23 06:47:55 +0800

    fix

--- synsin/train.py
+++ synsin/train.py
@@ -102,17 +102,20 @@ def train(epoch, data_loader, model, log_path, plotter, opts):
         if iteration % 250 == 0 or iteration == 1:
             for add_im in output_image.keys():
                 if iteration == 1 and os.environ['DEBUG']:
-                    if not os.path.isdir("./debug/Image_train/"):
-                        os.mkdir("./debug/Image_train/")
+
+                    pathFolder = "/home/abaozheng6/View_Synthesis/synsin/debug/Image_train/"
+                    if not os.path.isdir(pathFolder):
+                        os.mkdir(pathFolder)
 
                     torchvision.utils.save_image(
                         output_image[add_im][0:8, :, :, :].cpu().data,
                         "./debug/Image_train/%d_%s.png" % (iteration, add_im),
                         normalize=("Depth" in add_im),
                     )
-
-                if not os.path.isdir("Image_train/"):
-                    os.mkdir("Image_train/")
+                
+                pathFolder = "/home/abaozheng6/View_Synthesis/synsin/Image_train/"
+                if not os.path.isdir(pathFolder):
+                    os.mkdir(pathFolder)
                 
                 plotter.add_image(
                     "Image_train/%d_%s" % (iteration, add_im),
