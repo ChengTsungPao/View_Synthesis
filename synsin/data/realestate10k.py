508d721f0df8a76c09c36a329e21af1d3d081a41
chengtsungpao <abaozheng6@gmail.com>
2022-01-22 19:41:02 +0800

    fix

--- synsin/data/realestate10k.py
+++ synsin/data/realestate10k.py
@@ -26,7 +26,7 @@ class RealEstate10K(data.Dataset):
 
         if dataset == "train":
             print(self.imageset)
-            # self.imageset = self.imageset[0 : int(0.8 * self.imageset.shape[0])]
+            self.imageset = self.imageset[0 : int(0.8 * self.imageset.shape[0])]
         else:
             self.imageset = self.imageset[int(0.8 * self.imageset.shape[0]) :]
 
