c606173458f8756d41bccf46820f18ee1757b201
chengtsungpao <abaozheng6@gmail.com>
2022-01-22 20:32:54 +0800

    fix

--- synsin/data/realestate10k.py
+++ synsin/data/realestate10k.py
@@ -181,6 +181,8 @@ class RealEstate10K(data.Dataset):
         rgbs = []
         cameras = []
         for i in range(0, self.num_views):
+            print("loop i = {}".format(i))
+
             if i == 0:
                 t_index = image_index
             elif mask.shape[0] > 5:
