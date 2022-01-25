3d70b6a72a376f5af2175e6c10c6396d2454e3fe
chengtsungpao <abaozheng6@gmail.com>
2022-01-26 03:35:51 +0800

    fix

--- synsin/download.py
+++ synsin/download.py
@@ -1,4 +1,9 @@
 from pytube import YouTube
 
-yt = YouTube("")
-yt.streams.first().download()
+f = open("/home/abaozheng6/View_Synthesis/synsin/dataset/RealEstate10K/frames/train/0003a9bce989e532.txt", "r")
+url = f.readline()
+f.close()
+print(url)
+
+# yt = YouTube("")
+# yt.streams.first().download()
