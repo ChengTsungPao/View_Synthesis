b6d72f9c07646ec4017630e2ae3334941b7f7062
chengtsungpao <abaozheng6@gmail.com>
2022-01-26 02:59:28 +0800

    fix

--- synsin/clear.py
+++ synsin/clear.py
@@ -7,7 +7,10 @@ def printLine(files):
 
 path = "/home/abaozheng6/View_Synthesis/synsin/dataset/RealEstate10K/frames/train/"
 all_files = glob(path + "*")
+txt_files = glob(path + "*.txt")
 mp4_files = glob(path + "*.mp4")
 
-printLine(all_files)
-# printLine(mp4_files)
+# printLine(all_files)
+printLine(set(all_files) - set(txt_files))
+print("===============================================")
+printLine(mp4_files)
