ea2273654f73f2d6053a86442afeabdf487bd52f
chengtsungpao <abaozheng6@gmail.com>
2022-01-27 06:37:21 +0800

    fix

--- synsin/train.py
+++ synsin/train.py
@@ -93,7 +93,7 @@ def train(epoch, data_loader, model, log_path, plotter, opts):
     iter_data_loader = iter(data_loader)
 
     # Cheng Fix
-    iteration_number = 500 + 1
+    iteration_number = 1 + 1
     for iteration in range(1, min(iteration_number, len(data_loader))):
         date = time.localtime()
         print()
@@ -162,53 +162,61 @@ def val(epoch, data_loader, model, log_path, plotter):
     
     # Cheng Fix
     iteration_number = 10 + 1
-    for iteration in range(1, min(iteration_number, len(data_loader))):
+    # for iteration in range(1, min(iteration_number, len(data_loader))):
+    iteration = 1
+    while iteration < min(iteration_number, len(data_loader)):
         date = time.localtime()
         print()
         print(time.strftime("%Y-%m-%d %H:%M:%S", date))
         print("Validation iteration = {} (epoch = {})".format(iteration, epoch))
 
-        t_losses, output_image = model(
-            iter_data_loader, isval=True, num_steps=1
-        )
-        for l in t_losses.keys():
-            if l in losses.keys():
-                losses[l] = t_losses[l].cpu().mean().item() + losses[l]
-            else:
-                losses[l] = t_losses[l].cpu().mean().item()
-        if iteration % 100 == 0 or iteration == 1:
-            for add_im in output_image.keys():
-                
-                plotter.add_image(
-                    "Image_val/%d_%s" % (iteration, add_im),
-                    torchvision.utils.make_grid(
-                        output_image[add_im][0:8, :, :, :].cpu().data,
-                        normalize=("Depth" in add_im),
-                    ),
-                    epoch,
-                )
+        try:
 
-        if SIGNAL_RECEIVED:
-            trigger_job_requeue()
-            raise SystemExit
-
-        if iteration % 1 == 0:
-            str_to_print = "Val: Epoch {}: {}/{} with ".format(
-                epoch, iteration, len(data_loader)
+            t_losses, output_image = model(
+                iter_data_loader, isval=True, num_steps=1
             )
-            for l in losses.keys():
-                str_to_print += " %s : %0.4f | " % (
-                    l,
-                    losses[l] / float(iteration),
-                )
-            print(str_to_print, flush=True)
+            for l in t_losses.keys():
+                if l in losses.keys():
+                    losses[l] = t_losses[l].cpu().mean().item() + losses[l]
+                else:
+                    losses[l] = t_losses[l].cpu().mean().item()
+            if iteration % 100 == 0 or iteration == 1:
+                for add_im in output_image.keys():
+                    
+                    plotter.add_image(
+                        "Image_val/%d_%s" % (iteration, add_im),
+                        torchvision.utils.make_grid(
+                            output_image[add_im][0:8, :, :, :].cpu().data,
+                            normalize=("Depth" in add_im),
+                        ),
+                        epoch,
+                    )
 
-        for l in t_losses.keys():
-            plotter.add_scalars(
-                "%s_iter" % l,
-                {"val": t_losses[l].cpu().mean().detach().item()},
-                epoch * 50 + iteration,
-            )
+            if SIGNAL_RECEIVED:
+                trigger_job_requeue()
+                raise SystemExit
+
+            if iteration % 1 == 0:
+                str_to_print = "Val: Epoch {}: {}/{} with ".format(
+                    epoch, iteration, len(data_loader)
+                )
+                for l in losses.keys():
+                    str_to_print += " %s : %0.4f | " % (
+                        l,
+                        losses[l] / float(iteration),
+                    )
+                print(str_to_print, flush=True)
+
+            for l in t_losses.keys():
+                plotter.add_scalars(
+                    "%s_iter" % l,
+                    {"val": t_losses[l].cpu().mean().detach().item()},
+                    epoch * 50 + iteration,
+                )
+            iteration += 1
+        except:
+            pass
+            
             
     print("\n==================================================")
     return {l: losses[l] / float(iteration) for l in losses.keys()}
