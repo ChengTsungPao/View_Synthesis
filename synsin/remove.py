22dbb87a3373bd40a5e77cf95d1a484916cd61c8
chengtsungpao <abaozheng6@gmail.com>
2022-01-26 19:27:22 +0800

    fix

--- synsin/remove.py
+++ synsin/remove.py
@@ -4,7 +4,7 @@ from glob import glob
 path = "/home/abaozheng6/View_Synthesis/synsin/dataset/RealEstate10K/frames/train/"
 paths = glob(path + "*.mp4")
 
-removeNum = 3000
+removeNum = 2000
 for i in range(removeNum):
     mp4_file = paths[i]
     mp4_folder = paths[i].split(".mp4")[0]
