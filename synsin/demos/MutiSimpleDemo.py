e26d66e7796e7081a268ae6f03ec62a64e332802
chengtsungpao <abaozheng6@gmail.com>
2022-02-08 06:46:41 +0800

    fix

--- synsin/demos/MutiSimpleDemo.py
+++ synsin/demos/MutiSimpleDemo.py
@@ -264,7 +264,7 @@ def testAcc():
             plt.imshow(pred_imgs[0].squeeze().cpu().permute(1,2,0).numpy() * 0.5 + 0.5)
             plt.savefig("/home/abaozheng6/View_Synthesis/synsin/demos/test_image/pred/{}_{}_test_pred.png".format(file_txt, str(int(frame[0]))))
 
-            gtImagePath = "/home/abaozheng6/View_Synthesis/synsin/dataset/RealEstate10K/frames/train/{}/{}.png".format(file_txt, str(int(frames[index+1][0])))
+            gtImagePath = "/home/abaozheng6/View_Synthesis/synsin/dataset/RealEstate10K/frames/train/{}/{}.png".format(file_txt, str(int(frames[index][0])))
             lossfcn(im, transform(Image.open(gtImagePath)))
 
             if index == 30:
