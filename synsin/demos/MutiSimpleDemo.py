3ac57f5b7834292f6d10b5b3081c9a70bcf68258
chengtsungpao <abaozheng6@gmail.com>
2022-02-07 12:33:54 +0800

    fix

--- synsin/demos/MutiSimpleDemo.py
+++ synsin/demos/MutiSimpleDemo.py
@@ -235,7 +235,7 @@ def testAcc():
                 np.float32
             )
             P[3, 3] = 1
-            P = torch.tensor(P)
+            P = torch.tensor([P])
             RTS = [P] # [np.linalg.inv(P)]
             print(RTS)
 
