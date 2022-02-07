977e5f9a5999f89110bdede023de5ddaaad2f6d1
chengtsungpao <abaozheng6@gmail.com>
2022-02-08 07:13:33 +0800

    fix

--- synsin/demos/losses/ssim.py
+++ synsin/demos/losses/ssim.py
@@ -29,7 +29,7 @@ def create_window(window_size, channel):
 
 
 def _ssim(
-    img1, img2, window, window_size, channel, mask=None, size_average=True
+    img1, img2, window, window_size, channel, mask=None, size_average=False
 ):
     mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
     mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=channel)
