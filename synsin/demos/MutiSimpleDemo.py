91cf3c8dd57150d7d373fa7ef94af6941b004da4
chengtsungpao <abaozheng6@gmail.com>
2022-02-09 12:24:48 +0800

    fix

--- synsin/demos/MutiSimpleDemo.py
+++ synsin/demos/MutiSimpleDemo.py
@@ -272,7 +272,8 @@ def testAcc():
             plt.savefig("/home/abaozheng6/View_Synthesis/synsin/demos/test_image/pred/{}_{}_test_pred.png".format(file_txt, str(int(frame[0]))))
 
             gtImagePath = "/home/abaozheng6/View_Synthesis/synsin/dataset/RealEstate10K/frames/train/{}/{}.png".format(file_txt, str(int(frames[index][0])))
-            allLoss = lossfcn(pred_imgs[0].squeeze().cpu() * 0.5 + 0.5, transform(Image.open(gtImagePath)) * 0.5 + 0.5)
+            # allLoss = lossfcn(pred_imgs[0].squeeze().cpu() * 0.5 + 0.5, transform(Image.open(gtImagePath)) * 0.5 + 0.5)
+            allLoss = lossfcn(pred_imgs[0].squeeze().cpu() * 0.5 + 0.5, pred_imgs[1].squeeze().cpu() * 0.5 + 0.5)
 
             PSNR_DATA.append(allLoss[0])
             SSIM_DATA.append(allLoss[1])
