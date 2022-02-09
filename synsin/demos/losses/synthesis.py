a4d8e0bf25ede892d44857bedc070ca1d474cf06
chengtsungpao <abaozheng6@gmail.com>
2022-02-09 10:52:06 +0800

    fix

--- synsin/demos/losses/synthesis.py
+++ synsin/demos/losses/synthesis.py
@@ -74,7 +74,7 @@ class W_PSNR(nn.Module):
         # mse_err = ((pred_img - gt_img).pow(2) * weight).sum(dim=1).view(bs, -1).mean(dim=1) / np.sum(weight)
         a = (pred_img - gt_img) ** 2
         print(a.size(), weight.shape)
-        mse_err = np.sum(((pred_img - gt_img) ** 2) * weight) / np.sum(weight)
+        mse_err = np.sum(np.array((pred_img - gt_img) ** 2) * weight) / np.sum(weight)
 
         psnr = 10 * (1 / mse_err).log10()
         return {"psnr": psnr}
