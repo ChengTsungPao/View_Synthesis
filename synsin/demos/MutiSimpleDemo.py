84af6cab482fcccdde421ece1ec9710e964e6d4e
chengtsungpao <abaozheng6@gmail.com>
2022-02-09 14:01:32 +0800

    fix

--- synsin/demos/MutiSimpleDemo.py
+++ synsin/demos/MutiSimpleDemo.py
@@ -185,7 +185,7 @@ def testAcc():
         SSIM_DATA = []
         W_SSIM_DATA = []
 
-        for index in range(len(frames) - 1, len(frames)):
+        for index in range(0, len(frames) - 1):
 
             frame = frames[index]
             
@@ -240,13 +240,13 @@ def testAcc():
             rotation_inverse = np.linalg.inv(rotation)
             translation_inverse = -rotation_inverse @ translation
 
-            # if index == 0:
-            extrinsicMatrix = np.array([
-                [rotation[0, 0], rotation[0, 1], rotation[0, 2], translation[0]],
-                [rotation[1, 0], rotation[1, 1], rotation[1, 2], translation[1]],
-                [rotation[2, 0], rotation[2, 1], rotation[2, 2], translation[2]],
-                [           0.0,            0.0,            0.0,            1.0]
-            ]).astype(np.float32)
+            if index == 0:
+                extrinsicMatrix = np.array([
+                    [rotation[0, 0], rotation[0, 1], rotation[0, 2], translation[0]],
+                    [rotation[1, 0], rotation[1, 1], rotation[1, 2], translation[1]],
+                    [rotation[2, 0], rotation[2, 1], rotation[2, 2], translation[2]],
+                    [           0.0,            0.0,            0.0,            1.0]
+                ]).astype(np.float32)
 
             extrinsicMatrix_inverse = np.array([
                 [rotation_inverse[0, 0], rotation_inverse[0, 1], rotation_inverse[0, 2], translation_inverse[0]],
@@ -255,7 +255,7 @@ def testAcc():
                 [                   0.0,                    0.0,                    0.0,                    1.0]
             ]).astype(np.float32)
 
-            RTS = [torch.tensor([extrinsicMatrix_inverse])]
+            RTS = [torch.tensor([extrinsicMatrix * extrinsicMatrix_inverse])]
             # print(RTS)
 
             ###############################################
